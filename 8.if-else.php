81efdf67617ef68abcfd0a12d49fc7d1b9f32f4f
Sanmit Ghosal <60695034+001skg@users.noreply.github.com>
2025-12-25 23:32:09 +0530

    Create 8.if-else.php

---
+++ 8.if-else.php
@@ -0,0 +1,10 @@
+//5. Control Structures
+//Example 8: If-Else
+<?php
+$name="Alice";
+$marks = 75;
+if ($marks >= 40)
+echo "Hi $name! You are selected and Passed in the Exam";
+else
+echo "Hi $name! You are not selected and failed in the Exam.Try Again! Next Time you will be selected,don't worry.";
+?>
