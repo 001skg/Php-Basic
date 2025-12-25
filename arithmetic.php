42de90720ca373cd0e78b9429e3eb75ef5b3e11d
Sanmit Ghosal <60695034+001skg@users.noreply.github.com>
2025-12-25 23:21:16 +0530

    Create arithmetic.php

---
+++ arithmetic.php
@@ -0,0 +1,15 @@
+//4. Operators in PHP
+//Example 6: Arithmetic Operators
+<?php
+a = 20;
+b = 10;
+echo $a + $b;
+echo "<br>";
+echo $a - $b;
+echo "<br>";
+echo $a * $b;
+echo "<br>";
+echo $a / $b;
+echo "<br>";
+echo $a % $b;
+?>
